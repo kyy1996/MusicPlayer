a92b4b44121cffbd984eeec44d3ad136cdc7df19
system@kyy1996.cn <abc123>
2016-07-25 18:36:01 +0800

    Music API Operator

--- lyricx.php
+++ lyricx.php
@@ -6,9 +6,17 @@ class MusicAPIUtils
     var $API_LRC = "http://newlyric.kuwo.cn/newlyric.lrc?";
     var $API_LRC_RID = "http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=";
     var $API_SEARCH = 'http://search.kuwo.cn/r.s?ft=music&itemset=web_2013&client=kt&rformat=json&encoding=utf8&all={0}&pn={1}&rn={2}';
+    var $API_PLAY = "http://antiserver.kuwo.cn/anti.s?type=convert_url&format=aac|mp3&response=url&rid=";
 
     function __construct()
     {
+    }
+
+    public function getPlayUrl($music_rid)
+    {
+        $url = $this->API_PLAY . $music_rid;
+
+        return file_get_contents($url);
     }
 
     public function getMusicRid($name)
@@ -26,9 +34,9 @@ class MusicAPIUtils
      */
     public function getMusicList($name, $page = 0, $page_max = 10)
     {
-        $url = str_replace($this->API_SEARCH, '{0}', urlencode($name));
-        $url = str_replace($url, '{1}', $page);;
-        $url = str_replace($url, '{2}', $page_max);;
+        $url = str_replace('all={0}', "all=" . urlencode($name), $this->API_SEARCH);
+        $url = str_replace('{1}', $page, $url);;
+        $url = str_replace('{2}', $page_max, $url);
         $result = file_get_contents($url);
         $result = str_replace("'", "\"", $result);
         $list = json_decode($result, true);
@@ -37,6 +45,20 @@ class MusicAPIUtils
         }
 
         return $list;
+    }
+
+    public function getArtistImg($music_rid)
+    {
+        $url = $this->API_LRC_RID . $music_rid;
+        $xml = file_get_contents($url);
+
+        //查找歌手大图
+        preg_match("/<artist_pic240>(.+)<\/artist_pic240>/i", $xml, $result);
+        $img[] = $result[1];
+        //查找歌手小图
+        preg_match("/<artist_pic>(.+)<\/artist_pic>/i", $xml, $result);
+        $img[] = $result[1];
+        return $img;
     }
 
     /**
@@ -64,14 +86,13 @@ class MusicAPIUtils
         $url = $this->API_LRC_RID . $music_rid;
         $xml = file_get_contents($url);
         //查找逐字歌词
-//        preg_match("/<lyric_zz>(.+)<\/lyric_zz>/i", $xml, $result);
+        preg_match("/<lyric_zz>(.+)<\/lyric_zz>/i", $xml, $result);
         $lyricx = true;
         //逐字歌词不存在则查找普通歌词
-        $lyricx = false;
-//        if (!@$result[1]) {
-        $lyricx = false;
-        preg_match("/<lyric>(.+)<\/lyric>/i", $xml, $result);
-//        }
+        if (!@$result[1]) {
+            $lyricx = false;
+            preg_match("/<lyric>(.+)<\/lyric>/i", $xml, $result);
+        }
         if (!@$result[1])
             return false;
 
@@ -124,10 +145,15 @@ class MusicAPIUtils
 
 $Lyric = new MusicAPIUtils();
 $name = @$_REQUEST['name'] ? $_REQUEST['name'] : "刺藤";
-$lyric = $Lyric->getLyricRid($Lyric->getMusicRid($name));
-$str_lrc = $Lyric->getLyric($lyric['lyric'], $lyric['is_lrcx']);
-
-echo("<pre>");
+$music_rid = $Lyric->getMusicRid($name);
+$lyric = $Lyric->getLyricRid($music_rid);
+$str_lrc = $Lyric->getLyric($lyric['lyric_rid'], $lyric['is_lrcx']);
 
 header("Content-type:text/html;charset=gb18030");
-echo($str_lrc);
+echo("<pre>$str_lrc</pre>");
+
+$img = $Lyric->getArtistImg($music_rid);
+echo("<br>" . "<img src='{$img[0]}'>");
+echo("<br>" . "<img src='{$img[1]}'>");
+$aac = $Lyric->getPlayUrl($music_rid);
+echo("<br><audio src='{$aac}'></audio>");
